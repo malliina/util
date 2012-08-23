5553cd58a426da5efaf68e2c0d9c3340badf49eb
Michael Skogberg <malliina123@gmail.com>
2012-08-24 00:13:02 +0300

    lintian fixes

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,7 @@
 import sbt._
 import sbt.Keys._
 import com.typesafe.packager.PackagerPlugin._
-import com.typesafe.packager.{PackagerPlugin, linux, rpm, windows}
+import com.typesafe.packager.{PackagerPlugin, linux, debian, rpm, windows}
 
 /**
  * @author Mle
@@ -11,16 +11,23 @@ object GitBuild extends Build {
   lazy val parent = Project("parent", file("."))
   lazy val util = Project("common-util", file("common-util"), settings = mySettings)
   lazy val test = Project("test", file("test"), settings = mySettings ++ PackagerPlugin.packagerSettings).dependsOn(util).settings(
-    linux.Keys.maintainer := "Michael Skogberg",
+    // http://lintian.debian.org/tags/maintainer-address-missing.html
+    linux.Keys.maintainer := "Michael Skogberg <malliina123@ǧmail.com>",
     linux.Keys.packageSummary := "Test package summary",
-    linux.Keys.packageDescription := "Test package description",
+    linux.Keys.packageDescription := "This is the description of the test package",
     name := "test",
+    debian.Keys.version := "0.1",
     rpm.Keys.rpmRelease := "Release 0.1 for RPM",
-    rpm.Keys.rpmVendor := "King Michael",
+    rpm.Keys.rpmVendor := "kingmichael",
     windows.Keys.wixFile := new File("doesnotexist"),
     linux.Keys.linuxPackageMappings <+= baseDirectory map {
       bd => (packageMapping((bd / "dist" / "app.txt") -> "/opt/test/app.txt") withUser "root" withPerms "0644")
-    }
+    },
+    debian.Keys.linuxPackageMappings <+= (baseDirectory, name) map {
+      // http://lintian.debian.org/tags/no-copyright-file.html
+      (bd, pkgName) => (packageMapping((bd / "dist" / "copyright") -> ("/usr/share/doc/" + pkgName + "/copyright")) withUser "root" withPerms "0644")
+    },
+    debian.Keys.debianPackageDependencies in Debian ++= Seq("wget")
   )
 
   def mySettings = commonSettings
