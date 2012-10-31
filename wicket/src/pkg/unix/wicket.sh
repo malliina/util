2b0f7b6dc1861e3673dcbd5378c6045b3d947599
Michael Skogberg <malliina123@gmail.com>
2012-10-31 22:27:38 +0200

    bugfix

--- wicket/src/pkg/unix/wicket.sh
+++ wicket/src/pkg/unix/wicket.sh
@@ -54,7 +54,7 @@ case "$1" in
     start)
         if is_running; then
             echo "Already running"
-            exit 1
+            exit 0
         fi
         COMMAND="exec ${JAVA_CMD} ${JAVA_OPTS} -cp ${APP_HOME}/lib/*:${APP_HOME}/${APP_NAME}.jar ${MAIN_CLASS} >> ${APP_HOME}/logs/console.out 2>&1"
         if [ -z "${APP_USER}" ]; then
