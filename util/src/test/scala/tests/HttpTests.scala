0d5f6ae598e78da7e884867a146b3bffb2ed37c3
Michael Skogberg <michael.skogberg@futurice.com>
2017-10-29 13:00:34 +0200

    Fix tests

--- util/src/test/scala/tests/HttpTests.scala
+++ util/src/test/scala/tests/HttpTests.scala
@@ -1,7 +1,7 @@
 package tests
 
 import com.malliina.concurrent.ExecutionContexts.cached
-import com.malliina.http.AsyncHttp
+import com.malliina.http.{AsyncHttp, FullUrl}
 import org.scalatest.FunSuite
 
 import scala.concurrent.Await
@@ -9,7 +9,7 @@ import scala.concurrent.duration.DurationInt
 
 class HttpTests extends FunSuite {
   test("make request") {
-    val f = AsyncHttp.get("http://www.google.com")
+    val f = AsyncHttp.get(FullUrl.build("http://www.google.com").right.get)
     val res = Await.result(f, 10.seconds)
     assert(res.isSuccess)
     assert(res.asString.nonEmpty)
